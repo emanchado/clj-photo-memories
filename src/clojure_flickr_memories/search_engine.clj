d0ee5e100bf3ae7f8e4ba0c24d6fee0210cab357
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2013-10-07 15:57:30 +0200

    Make search-photos receive optional parameters
    * The optional parameters allow the caller to use a different
      base URL or application key
    * While on it, rename the constants with the base URL and
      application key *base-url* and *api_key*, and make them dynamic

--- src/clojure_flickr_memories/search_engine.clj
+++ src/clojure_flickr_memories/search_engine.clj
@@ -4,8 +4,8 @@
             [clj-http.client :as http-client])
   (:use clojure.contrib.zip-filter.xml))
 
-(def api-key "30c195ccce757cd281132f0bef44de0d")
-(def base-url "http://api.flickr.com/services/rest")
+(def ^:dynamic *api-key* "30c195ccce757cd281132f0bef44de0d")
+(def ^:dynamic *base-url* "http://api.flickr.com/services/rest")
 
 (defn photos-in-xml-result [xml]
   (let [java-stream (java.io.ByteArrayInputStream. (.getBytes xml))
@@ -22,7 +22,9 @@
               :description (first (:content (first (filter #(= (:tag %) :description) (:content (first photo-zipped-xml))))))}))
          photo-list)))
 
-(defn search-photos [user-id from-date to-date]
+(defn search-photos [user-id from-date to-date & {:keys [base-url api-key]
+                                                  :or {base-url *base-url*
+                                                       api-key *api-key*}}]
   (let [response (http-client/get base-url
                                   {:query-params {:method "flickr.photos.search"
                                                   :api_key api-key
