039753065b0f77d15d0d32bb5bd3aa86f97dc94f
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2014-12-20 12:26:31 +0100

    Include first day of week in Trovebox results
    The Trovebox API does NOT include results for the date in takenAfter,
    so we have to substract one day from that date before sending it to
    the API.

--- src/clj_photo_memories/trovebox_client.clj
+++ src/clj_photo_memories/trovebox_client.clj
@@ -1,6 +1,7 @@
 (ns clj-photo-memories.trovebox-client
+  (:import java.util.Calendar java.text.SimpleDateFormat)
   (:require [clj-photo-memories.service-protocol :as service-protocol]
-            [clj-http.client :as http-client]
+            [clj-http.client :as http]
             [clojure.data.json :as json]))
 
 (defrecord TroveboxClient [options])
@@ -21,14 +22,23 @@
                      :thumbnail-url (get result "path500x300")})))
          results)))
 
+(defn day-before [date]
+  (let [rfc3339-formatter (SimpleDateFormat. "yyyy-MM-dd")
+        calendar (Calendar/getInstance)]
+    (.setTime calendar (.parse rfc3339-formatter date))
+    (.add calendar Calendar/DATE -1)
+    (.format rfc3339-formatter (.getTime calendar))))
+
 (extend-type TroveboxClient service-protocol/ServiceClient
              (search-photos [this username from-date to-date]
                (try
                  (let [base-url (:base-url (:options this))
-                       response (http-client/get (str base-url "/photos/list.json")
-                                                 {:query-params {:returnSizes "500x300"
-                                                                 :takenBefore to-date
-                                                                 :takenAfter from-date}})]
+                       query-params {:query-params {:returnSizes "500x300"
+                                                    :takenAfter (day-before
+                                                                 from-date)
+                                                    :takenBefore to-date}}
+                       response (http/get (str base-url "/photos/list.json")
+                                          query-params)]
                    (photos-in-json-result (:body response)))
                  (catch Exception e
                    (println (str "Couldn't get photos from the Trovebox server at " (:base-url (:options this)) " for " from-date "-" to-date))))))
